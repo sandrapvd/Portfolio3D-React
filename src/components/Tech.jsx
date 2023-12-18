34b08f2a92f67c94fb2641ffd429f73fe12a9be9
sandrapvd <sandra_pvd@hotmail.es>
2023-12-18 11:01:43 +0100

    First commit with application

---
+++ src/components/Tech.jsx
@@ -0,0 +1,24 @@
+import { BallCanvas } from "./canvas"
+import { SectionWrapper } from "../hoc"
+import { technologies } from "../constants"
+
+const Tech = () => {
+  return (
+    <div className="flex flex-row flex-wrap justify-center
+    gap-10">
+      {technologies.map((technologie, index) => (
+        
+        <div 
+          className="w-28 h-28"
+          key = {technologie.name}
+        >
+          <BallCanvas 
+            icon={technologie.icon}/>
+        </div>
+
+      ))}
+    </div>
+  )
+}
+
+export default Tech
